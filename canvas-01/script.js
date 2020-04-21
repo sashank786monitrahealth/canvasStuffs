da97d413b47a1daf837d2bd86205bca10b8b8cac
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-04-21 20:50:54 +0530

    setup ctx variables

--- canvas-01/script.js
+++ canvas-01/script.js
@@ -3,3 +3,15 @@ const ctx = canvas.getContext("2d");
 const penColor = document.querySelector('input[name="penColor"]');
 const penWidth = document.querySelector('input[name="penWidth"]');
 const saver = document.querySelector("#saver");
+
+
+ctx.strokeStyle = "#000000";
+ctx.lineJoin = "round";
+ctx.lineCap ="round";
+ctx.lineWidth = 5;
+
+let pen = {
+    x:0,
+    y:0,
+    down:false
+}
