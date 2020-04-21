950404632914b370f4d0e1d459dae63ada7f9027
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-04-21 20:46:32 +0530

    added querySelector

---
+++ canvas-01/script.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+const penColor = document.querySelector('input[name="penColor"]');
+const penWidth = document.querySelector('input[name="penWidth"]');
+const saver = document.querySelector("#saver");
