ecbb2ac4aa5063d9cce9e672bc2974d15c15b47f
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-04-21 21:08:54 +0530

    add Event Listeners

--- canvas-01/script.js
+++ canvas-01/script.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 const penColor = document.querySelector('input[name="penColor"]');
 const penWidth = document.querySelector('input[name="penWidth"]');
 const saver = document.querySelector("#saver");
@@ -14,4 +14,30 @@ let pen = {
     x:0,
     y:0,
     down:false
+}
+
+saver.addEventListener('click',saveFile);
+canvas.addEventListener('mousedown',penDown);
+canvas.addEventListener('mousemove',draw);
+canvas.addEventListener('mouseup',noDown);
+canvas.addEventListener('mouseout',noDown);
+
+
+function noDown(){
+    console.log('out');
+    pen.down = false;
+}
+
+
+
+function draw(){
+  console.log('move');
+}
+
+function penDown(){
+   console.log('down');
+}
+
+function saveFile(){
+
 }
