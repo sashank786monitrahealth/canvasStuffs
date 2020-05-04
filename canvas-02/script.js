75d604ea9b5c2eb14a323f0b66aaaaef84a862ba
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-04 16:03:40 +0530

    set context before shapes

--- canvas-02/script.js
+++ canvas-02/script.js
@@ -2,7 +2,12 @@ var canvas = document.getElementById("myCan");
 var ctx = canvas.getContext("2d");
 
 // short hand to create rect
+ctx.fillStyle="yellow";
+ctx.strokeStyle = "lime";
+ctx.lineWidth ="10";
+ctx.strokeRect(100,100,100,100);
 ctx.fillRect(100,100,100,100);
+ctx.lineWidth ="5";
 ctx.strokeRect(100,250,100,100);
 
 // long form to create rect
@@ -14,4 +19,4 @@ ctx.lineWidth = "5";
 ctx.stroke();
 
 // clearRect is used to clear pixels
-ctx.clearRect(0,50,500,100);
+//ctx.clearRect(0,50,640,480);
