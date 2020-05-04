f51a246dfb2eec8eeba5688acd65ab5a7eab76f4
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-04 14:59:13 +0530

    shorthand notation vs long form

--- canvas-02/script.js
+++ canvas-02/script.js
@@ -1,3 +1,14 @@
 var canvas = document.getElementById("myCan");
 var ctx = canvas.getContext("2d");
-ctx.fillRect(100,100,440,280);
+
+// short hand to create rect
+ctx.fillRect(100,100,100,100);
+ctx.strokeRect(100,250,100,100);
+
+// long form to create rect
+ctx.rect(300,100,100,100);
+ctx.fillStyle="blue";
+ctx.fill();
+ctx.strokeStyle = "lime";
+ctx.lineWidth = "5";
+ctx.stroke();
