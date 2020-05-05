91c7f638540cefa66b1a64adae964226847b50ea
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-05-05 13:04:11 +0530

    stroke Text

--- canvas-03/script.js
+++ canvas-03/script.js
@@ -2,4 +2,10 @@ var canvas = document.getElementById("myCan");
 var ctx = canvas.getContext("2d");
 
 ctx.font = "48px Arial";
+ctx.fillStyle = "lime";
 ctx.fillText("first text",50,50);
+//
+
+ctx.strokeStyle ="dodgerblue"
+ctx.strokeText("second Text",50,100);
+
